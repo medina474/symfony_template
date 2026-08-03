4b3bc4f5f5f5fd169d3e38923cfe4a065deef019
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2026-08-03 10:18:06 +0200

    composer: Composants de développement symfony/maker-bundle, symfony/debug-pack, symfony/test-pack, phpstan/phpstan-symfony

--- webapp.sh
+++ webapp.sh
@@ -2,3 +2,10 @@
 
 composer require \
 	symfony/monolog-bundle
+
+# Composants de développement
+composer require --dev \
+	symfony/maker-bundle \
+	symfony/debug-pack \
+	symfony/test-pack \
+	phpstan/phpstan-symfony
